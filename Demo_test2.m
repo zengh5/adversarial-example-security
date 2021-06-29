26a3532d4a8d50fd1ed1c98c3a81d3627821143f
Hui Zeng <34535559+zengh5@users.noreply.github.com>
2021-06-29 19:16:18 +0800

    Update Demo_test2.m

--- Demo_test2.m
+++ Demo_test2.m
@@ -12,7 +12,7 @@ load Test2/model_51_ori_mixed_PGDresave.mat
 load Test2/Threshold_fpr2PGDresave.mat
 threshold = (Threshold_fpr2(P_fa2/0.01)+Threshold_fpr2(P_fa2/0.01+1))/2;
 
-% 3 read the image ans extract SRM features
+% 3 read the image and extract SRM features
 sourcefile = 'sample/PGD01/demo.png';
 % sourcefile = 'sample/PGD04/demo.png';
 % sourcefile = 'sample/ORI/demo.png';
@@ -20,8 +20,8 @@ fea       = SRM34671(sourcefile);
 
 % 4 Classify the features with a pre-trained ensemble classifier
 test_results = ensemble_testing(fea,trained_ensemble)
-%% test_results: the classification results of N sub classifiers 
-%% We do not use the 'prediction' variable directly as that done in steganalysis 
+%% test_results: the classification results of N sub classifiers, here N=51 
+%% We do not use the 'prediction' variable directly as that done in steganalysis
 if test_results.votes <= threshold
     disp('The image is benign'),
 else
